512f7aa6aef5915592a2c328ef3b7071e3723d3c
Nowshad123 <nowshadahmed003@gmail.com>
2022-03-21 23:46:43 +0600

    Initial commit

---
+++ project.php
@@ -0,0 +1,39 @@
+<?php
+////$numbers = array(4,36,9,6,42);
+////echo $numbers[4];?><!--<!-- <br>-->-->
+<?php
+////$count = 2;
+////echo $count +=1;
+////?><!--<!--<br/>-->-->
+<?php
+//$x = 1;
+////if (++$x == 10);
+////echo $x;
+////?>
+<?php
+//$number = 12345 * 67890;
+//echo substr($number, 3, 1);
+//?>
+<!--<br>-->
+<?php
+//define("Name", "Nowshad");
+//echo Name;
+//?>
+<?php
+//$name = "nowshad";
+//$college_name ="BAFSD";
+//printf("Name %s My college name %s", $name,$college_name);
+//?>
+<?php
+$username = "Nowshad";
+$password = 1234 ;
+
+if ($username = "Nowshad" && $password = 1234){
+    echo "all ok";
+    if($username="Nowshad"){
+        echo "incorrect password";
+    }
+} else{
+    echo "invelid username/password";
+}
+?>
